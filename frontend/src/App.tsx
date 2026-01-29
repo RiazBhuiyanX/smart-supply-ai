bf110ad3068876e6df654cc95d5cf7135261c3a4
Riaz Bhuiyan <riaz.s.bhuiyan@gmail.com>
2026-01-30 00:04:18 +0200

    feat(frontend): add Warehouses and Inventory pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { LoginPage } from '@/pages/LoginPage'
 import { DashboardPage } from '@/pages/DashboardPage'
 import { ProductsPage } from '@/pages/ProductsPage'
+import { WarehousesPage } from '@/pages/WarehousesPage'
+import { InventoryPage } from '@/pages/InventoryPage'
 import './index.css'
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/products" element={<ProductsPage />} />
+        <Route path="/warehouses" element={<WarehousesPage />} />
+        <Route path="/inventory" element={<InventoryPage />} />
         <Route path="/" element={<DashboardPage />} />
       </Routes>
     </BrowserRouter>
