4d52f386fc6b45f9386f258038a20d5cbd004795
Riaz Bhuiyan <riaz.s.bhuiyan@gmail.com>
2026-02-04 03:36:57 +0200

    feat(frontend): add Suppliers, PurchaseOrders, AuditTrail pages - Closes #28

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import { DashboardPage } from '@/pages/DashboardPage'
 import { ProductsPage } from '@/pages/ProductsPage'
 import { WarehousesPage } from '@/pages/WarehousesPage'
 import { InventoryPage } from '@/pages/InventoryPage'
+import { SuppliersPage } from '@/pages/SuppliersPage'
+import { PurchaseOrdersPage } from '@/pages/PurchaseOrdersPage'
+import { InventoryMovementsPage } from '@/pages/InventoryMovementsPage'
 import './index.css'
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/warehouses" element={<WarehousesPage />} />
         <Route path="/inventory" element={<InventoryPage />} />
+        <Route path="/suppliers" element={<SuppliersPage />} />
+        <Route path="/purchase-orders" element={<PurchaseOrdersPage />} />
+        <Route path="/inventory-movements" element={<InventoryMovementsPage />} />
         <Route path="/" element={<DashboardPage />} />
       </Routes>
     </BrowserRouter>
